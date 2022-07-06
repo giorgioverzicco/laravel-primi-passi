27f113c4fb6e6592e3cf3d6b8e75316b02dbbee6
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-07-06 13:18:21 +0200

    add some pages

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<header class="py-4 px-8 bg-gray-900 shadow-lg">
+    <nav>
+        <ul class="flex items-center justify-end gap-x-8 text-lg text-white font-medium">
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/chi-siamo">Chi Siamo</a>
+            </li>
+            <li>
+                <a href="/contatti">Contatti</a>
+            </li>
+        </ul>
+    </nav>
+</header>
--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <div class="max-w-3/4 pt-16 overflow-hidden grid place-items-center">
         <div>
-            <h1 class="text-4xl font-bold">Ciao Laravel!</h1>
+            <h1 class="text-4xl font-bold underline">Route Contatti</h1>
         </div>
     </div>
 </x-layout>
